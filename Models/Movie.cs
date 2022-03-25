f62ebe417210bc44aed23ca6aac13fe217bc5b32
irisy <ayao@my.wctc.edu>
2022-03-25 15:11:33 -0500

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MovieLibrary.Models
+{
+    public class Movie : Media
+    {
+        public string Genres { get; set; }
+
+        public override string ToString()
+        {
+            return $"Movie: {Title}, {Genres}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MovieLibrary.Services;
+
+namespace MovieLibrary
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var orchestrator = new MediaOrchestrator();
+
+            Console.Write("Enter the search string(enter 'my' to test): ");
+            var searchString = Console.ReadLine().ToLower();
+            var results = orchestrator.SearchAllMedia(searchString);
+
+            Console.WriteLine("Your searching results are:");
+            results.ForEach(Console.WriteLine);
+
+        }
+    }
+}
